8414e69986d2c0c0658ac91b9b57fd1097e95545
deppy <emilio.depero@cern.ch>
2020-03-10 20:45:04 +0100

    now compress all emacs directory

--- fetch.sh
+++ fetch.sh
@@ -7,6 +7,10 @@ cp -v $EMACSLOCATION/myinit.org emacs/
 cp -v $UNIXLOCATION/.setup.org unix/
 cp -v $UNIXLOCATION/.setup.sh unix/
 cp -v $UNIXLOCATION/.bashrc unix/
+
 #list user installed package
 dnf history userinstalled > unix/packageinstalled.txt
 ls -ltr /opt > unix/softwareinstalled.txt
+
+#compress all emacs file
+tar -czf emacs/emacs.tar $EMACSLOCATION 
